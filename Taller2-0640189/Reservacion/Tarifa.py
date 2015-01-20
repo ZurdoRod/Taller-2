8bd64f97866f566280c68b6ee293b5a11b2077e7
ZurdoRod <ZurdoRod@gmail.com>
2015-01-19 22:40:31 -0430

    clase Tarifa

---
+++ Taller2-0640189/Reservacion/Tarifa.py
@@ -0,0 +1,16 @@
+'''
+Created on 19/1/2015
+
+@author: ZurdoRod
+'''
+
+class Tarifa(object):
+
+    tarifaDiurna = 0
+    tarifaNocturna = 0
+    
+
+    def __init__(self, tarifaDiurna, tarifaNocturna):
+        self.tarifaDiurna = tarifaDiurna
+        self.tarifaNocturna = tarifaNocturna
+        
