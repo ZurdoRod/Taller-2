93fcd5d03a04da9d9d5c006f88be337dee1d39eb
ZurdoRod <ZurdoRod@gmail.com>
2015-01-20 08:19:52 -0430

    modificacion del calculo del costo por funcion recursiva (aun falta)

--- Taller2-0640189/Reservacion/funciones.py
+++ Taller2-0640189/Reservacion/funciones.py
@@ -6,24 +6,52 @@ Created on 19/1/2015
 from datetime import datetime
 from Reservacion import Tarifa
 
+def aumentarDia (fechaIn):
+    dia = fechaIn.date().day
+    mes = fechaIn.date().month
+    anio = fechaIn.date().year
+    if dia > 0 and dia < 28:
+        return fechaIn.replace(day = dia + 1)
+    elif dia == 29 and mes != 2:
+        return fechaIn.replace(day = dia + 1)
+    elif dia == 30 and (mes != 4 and mes != 6 and mes != 9 and mes != 11):
+        return fechaIn.replace(day = dia + 1)
+    elif dia == 31 and mes != 12:
+        return fechaIn.replace(month = mes + 1, day = 1)
+    elif dia == 31 and mes == 12:
+        return fechaIn.replace(year = anio + 1, month = 1, day = 1)
+    elif dia == 30 and (mes == 4 or mes == 6 or mes == 9 or mes == 11):
+        return fechaIn.replace(month = mes + 1, day = 1)
+    elif dia == 29 and mes == 2:
+        return fechaIn.replace(month = mes + 1, day = 1)
+    elif dia == 28 and mes == 2 and (anio % 4 == 0 or (anio % 100 == 0 and anio % 400 != 0)):
+        return fechaIn.replace(day = 29)
+    else:
+        return fechaIn.replace(month = mes + 1, day = 1)
+        
+def calcularDia (fechaIn, fechaOut, fechaDia, tarifa, acum):
+    return 0    #FALTA
+        
+    
+
 def calcularCosto (fechaIn, fechaOut, tarifa):
     if fechaIn  > fechaOut:
         return 'ERROR! fecha de salida menor que fecha de entrada'
     diferencia = fechaOut - fechaIn
-    if diferencia < (datetime.strptime('2000-1-1 00:15', '%Y-%m-%d %H:%M') - datetime.strptime('2000-1-1 00:00', '%Y-%m-%d %H:%M')):
+    if diferencia < (datetime.strptime('1-1-2012 00:15', '%d-%m-%Y %H:%M') - datetime.strptime('1-1-2012 00:00', '%d-%m-%Y %H:%M')):
         return 'ERROR! La reservacion es menor a 15 minutos'
-    if diferencia > (datetime.strptime('2000-1-1 00:15', '%Y-%m-%d %H:%M') - datetime.strptime('2000-1-3 00:15', '%Y-%m-%d %H:%M')):
+    if diferencia > (datetime.strptime('4-1-2000 00:15', '%d-%m-%Y %H:%M') - datetime.strptime('1-1-2000 00:15', '%d-%m-%Y %H:%M')):
         return 'ERROR! La reservacion es mayor a 72 horas'
     
-    
+    return calcularDia(fechaIn, fechaOut, aumentarDia(fechaIn), tarifa, 0)
         
 
 def reservacion (fechaIn, fechaOut, costoD, costoN):
     try:
-        fecha1 = datetime.strptime(fechaIn, '%Y-%m-%d %H:%M')
-        fecha2 = datetime.strptime(fechaOut, '%Y-%m-%d %H:%M')
+        fecha1 = datetime.strptime(fechaIn, '%d-%m-%Y %H:%M')
+        fecha2 = datetime.strptime(fechaOut, '%d-%m-%Y %H:%M')
     except ValueError:
-        return 'ERROR! en las fechas introducidas'
+        return 'ERROR! Fechas incorrectas'
     
     if costoD<0 or costoN<0:
         return 'ERROR! Costos no pueden ser negativos'
@@ -31,5 +59,4 @@ def reservacion (fechaIn, fechaOut, costoD, costoN):
         tarifa = Tarifa.Tarifas (costoD, costoN)
         return calcularCosto(fecha1, fecha2, tarifa)
     
-        
         
--- Taller2-0640189/modPruebas/mdlPruebas.py
+++ Taller2-0640189/modPruebas/mdlPruebas.py
@@ -4,17 +4,41 @@ Created on 19/1/2015
 @author: ZurdoRod
 '''
 import unittest
+from datetime import datetime
 from Reservacion import funciones
 
 class Test(unittest.TestCase):
+    
+#    def test03 (self):
+#        self.assertEqual(80, funciones.reservacion('13-10-2014 2:00', '13-10-2014 8:45', 20.0, 10.0), 'diurna-nocturna')
+    
+    def test02 (self):
+        self.assertEqual(80, funciones.reservacion('13-10-2014 2:00', '13-10-2014 8:00', 20.0, 10.0), 'diurna-nocturna')
 
+    def test01 (self):
+        self.assertEqual(30, funciones.reservacion('13-10-2014 2:30', '13-10-2014 4:35', 20.0, 10.0), 'solo nocturna')
+        
+    def testDiaMasMas4 (self):
+        self.assertEqual(datetime.strptime('2-5-2012 00:15', '%d-%m-%Y %H:%M'), funciones.aumentarDia(datetime.strptime('1-5-2012 00:15', '%d-%m-%Y %H:%M')), 'Aumentar un dia a mes')
+        
+    def testDiaMasMas3 (self):
+        self.assertEqual(datetime.strptime('1-6-2012 00:15', '%d-%m-%Y %H:%M'), funciones.aumentarDia(datetime.strptime('31-5-2012 00:15', '%d-%m-%Y %H:%M')), 'Aumentar un dia a mes de 31')
+        
+    def testDiaMasMas2 (self):
+        self.assertEqual(datetime.strptime('1-5-2012 00:15', '%d-%m-%Y %H:%M'), funciones.aumentarDia(datetime.strptime('30-4-2012 00:15', '%d-%m-%Y %H:%M')), 'Aumentar un dia a mes de 30')
+        
+    def testDiaMasMas (self):
+        self.assertEqual(datetime.strptime('1-1-2012 00:15', '%d-%m-%Y %H:%M'), funciones.aumentarDia(datetime.strptime('31-12-2011 00:15', '%d-%m-%Y %H:%M')), 'Aumentar un dia a diciembre')
+        
+    def testfechaIncorrecta(self):
+        self.assertEqual('ERROR! Fechas incorrectas', funciones.reservacion('1-0-1 00:10', '2-4-2000',  40.0,   20.0), 'mes invalido')
 
     def testfechaMenor(self):
-        self.assertEqual('ERROR! La reservacion es menor a 15 minutos', funciones.reservacion('2012-12-12 01:20','2012-12-12 01:30', 12.0, 20.0), 'La reservacion es menor a 15 minutos')
+        self.assertEqual('ERROR! La reservacion es menor a 15 minutos', funciones.reservacion('12-12-2012 01:20','12-12-2012 01:30', 12.0, 20.0), 'La reservacion es menor a 15 minutos')
         
     def testfechaMayor(self):
-        self.assertEqual('ERROR! La reservacion es mayor a 72 horas', funciones.reservacion('2012-12-12 01:20','2012-12-12 01:30', 12.0, 20.0), 'La reservacion es mayor a 72 horas')
-
+        self.assertEqual('ERROR! La reservacion es mayor a 72 horas', funciones.reservacion('12-12-2012 01:20','15-12-2012 01:30', 12.0, 20.0), 'La reservacion es mayor a 72 horas')
+        
 
 
 if __name__ == "__main__":
